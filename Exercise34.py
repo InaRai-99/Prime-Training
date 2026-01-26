b2493179e7296971326adfcad0aed1b2ce5189be
Ina Rai <inarai148@gmail.com>
2026-01-26 12:25:48 +0000

    Ex 4

--- Exercise34.py
+++ Exercise34.py
@@ -1,1 +1,5 @@
-User = {'id': id_obj, 'name':name_obj,'roles':role_obj}
+User ------------> {'id': id_obj, 'name':name_obj,'roles':role_list}
+id_obj ----------->101
+name_obj ---------> Alice
+roles_list -------->["editor","viewer"]
+
