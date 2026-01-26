9685010e6b9b0c28a7a1782b6b15dbc11db008f4
Ina Rai <inarai148@gmail.com>
2026-01-26 13:53:25 +0000

    Examples

---
+++ Ifelse.py
@@ -0,0 +1,5 @@
+Age = 60
+if (Age>70):
+    print("You are a senior citizen.")
+else:
+    print("You are not a senior citizen.")
---
+++ Tableofn.py
@@ -0,0 +1,4 @@
+n=int(input("enter any number"))
+#n=4
+for i in range (1,20):
+    print(n, "X", i, "=", n*i)
