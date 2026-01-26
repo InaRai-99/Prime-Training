ea31e14967c7fa93f4a5ec7bfce526b38cf13909
Ina Rai <inarai148@gmail.com>
2026-01-26 11:08:59 +0000

    new list

--- Lists.py
+++ Lists.py
@@ -1,11 +1,22 @@
-Students= ["Ant", "Cat", "Hank", "Tom", "Ron"]
+Username = "Leo"
+print (Username)
+Username = "Romeo"
+print (Username)
+
+Pupil= ["Ant", "Cat", "Hank", "Tom", "Ron"]
+print("First pupil is", Pupil[1])
+Pupil [1]="Garry"
+print("First pupil is", Pupil[1])
+print(Pupil)
+Pupil=["a","b","c"]
+print(Pupil)
 Marks= [60, 70, 80, 85, 50]
 a = ["Demo", 156, 466, "Hello"]
-print (Students)
-print (Marks)
-print (a)
-print ("First Student is", Students[0])
-print ("Second Student is", Students[1])
-print ("Third Student is", Students[2])
-print ("Fourth Student is", Students[3])
-print ("Fifth Student is", Students[4])
+#print (Students)
+#print (Marks)
+#print (a)
+#print ("First Student is", Students[0])
+#print ("Second Student is", Students[1])
+#print ("Third Student is", Students[2])
+#print ("Fourth Student is", Students[3])
+#print ("Fifth Student is", Students[4])
