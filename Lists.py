dd97a002ae59b14e5a89109ae2b1dcf648edfbfd
Ina Rai <inarai148@gmail.com>
2026-01-26 10:05:02 +0000

    Create Lists.py

---
+++ Lists.py
@@ -0,0 +1,11 @@
+Students= ["Ant", "Cat", "Hank", "Tom", "Ron"]
+Marks= [60, 70, 80, 85, 50]
+a = ["Demo", 156, 466, "Hello"]
+print (Students)
+print (Marks)
+print (a)
+print ("First Student is", Students[0])
+print ("Second Student is", Students[1])
+print ("Third Student is", Students[2])
+print ("Fourth Student is", Students[3])
+print ("Fifth Student is", Students[4])
