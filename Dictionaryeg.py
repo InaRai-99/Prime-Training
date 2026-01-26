636789b36cb7c4425f5cc9870941f3a5c8798dce
Ina Rai <inarai148@gmail.com>
2026-01-26 11:31:25 +0000

    New File

---
+++ Dictionaryeg.py
@@ -0,0 +1,6 @@
+Student1 = {"Name":"Josh", 
+            "City":"NYC", 
+            "Age":"35"}
+print(Student1["Name"])
+Student1["Name"]="Ken"
+print(Student1["Name"])
---
+++ Setseg.py
@@ -0,0 +1,5 @@
+set1 = {6,7,8,9,10}
+print(set1)
+list1 =["Apple", "Banana", "Grapes", "Mango", "Jackfruit", "Orange"]
+Fruits_Set=set(list1)
+print(Fruits_Set)
